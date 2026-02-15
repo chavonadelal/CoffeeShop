a62743551832af8c2e5dac8b4efcb6c64ea8dfd2
chavonadelal <chavonadelal@mail.ru>
2026-02-15 04:37:59 +0300

    OrderItemDto valid

--- CoffeeShop.Api/DTOs/CreateOrderDto.cs
+++ CoffeeShop.Api/DTOs/CreateOrderDto.cs
@@ -1,13 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CoffeeShop.Api.DTOs
 {
     public class OrderItemDto 
     {
+        [Required(ErrorMessage = "Product ID is required")]
         public Guid ProductId { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Quantity must be between 1 and 1000")]
         public int Quantity { get; set; }
     }
 
     public class CreateOrderDto 
     {
+        [Required]
+        [MinLength(1, ErrorMessage = "Order must contain at least one item")]
         public List<OrderItemDto> Items { get; set; } = new();
     }
 }
